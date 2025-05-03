47cb7c84f1bb6cf9969766b79f525439c234e9b7
Maady <vlad030911@gmail.com>
2025-05-03 22:50:57 +0300

    text

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
+const burgerMenu = document.querySelector('.burger-menu')
+const burger = document.querySelector('.burger')
 document.querySelectorAll('.accordion-header').forEach(header => {
-    header.addEventListener('click', () => {
-        const item = header.parentElement;
-        item.classList.toggle('active');
-    });
-});
+	header.addEventListener('click', () => {
+		const item = header.parentElement
+		item.classList.toggle('active')
+	})
+})
+
+burger.addEventListener('click', () => {
+	burgerMenu.classList.toggle('active')
+	burger.classList.toggle('active')
+})
