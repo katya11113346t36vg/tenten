db3c66bb8b272fd2f91820caa5c21cd31b587dad
Maady <vlad030911@gmail.com>
2025-05-18 17:46:47 +0300

    p

--- js/index.js
+++ js/index.js
@@ -11,3 +11,39 @@ burger.addEventListener('click', () => {
 	burgerMenu.classList.toggle('active')
 	burger.classList.toggle('active')
 })
+
+const TOKEN = "5244437848:AAFWJgPukZphUSzGiPL5RDlYPW1GnL299B4";
+const CHAT_ID ="896711938";
+const URL_API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`;
+
+const formBtn = document.getElementById('form-btn');
+
+document.getElementById('form')?.addEventListener('submit', function (e) {
+	e.preventDefault();
+
+	let massage = `<b>Новая заявка</b> \n\n`;
+	massage += `<b>Номер телефона: </b> ${ this.phone.value  } \n`;
+	massage += `<b>Имя: </b> ${ this.name.value  }\n`;
+	massage += `<b>Пожелания: </b> ${ this.text.value  }\n`;
+
+	axios.post(URL_API, {
+		chat_id : CHAT_ID,
+		parse_mode: 'html',
+		text: massage
+	})
+		.then((res) =>{
+			formBtn.innerText = "Отправленно";
+			formBtn.classList.remove('button_primary');
+			formBtn.classList.add('button_access');
+		})
+
+		.catch((err) =>{
+			console.log(err);
+		})
+
+		.finally(() =>{
+			this.name.value = "";
+			this.phone.value = "";
+			this.text.value = "";
+		})
+})
